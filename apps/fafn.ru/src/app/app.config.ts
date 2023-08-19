941e3971b49aa6eb125c2379435cad02e203422e
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-19 15:41:58 +0700

    [app] Adding meta and metrics

--- apps/fafn.ru/src/app/app.config.ts
+++ apps/fafn.ru/src/app/app.config.ts
@@ -1,5 +1,7 @@
-import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
+import { APP_ID, APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+
+import { META_CONFIG, MetaConfig, MetaService, METRIC_CONFIG, MetricConfig, MetricService } from '@fafnur/core';
 
 // import { MetaService, MetricService } from '@angular-blog/core';
 import { appRoutes } from './app.routes';
@@ -15,16 +17,41 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     {
+      provide: APP_ID,
+      useValue: 'fafnur',
+    },
+    {
+      provide: META_CONFIG,
+      useValue: {
+        hostname: 'https://fafn.ru',
+        siteName: 'Fafnur',
+        image: '/assets/images/site.jpg',
+        imageType: 'image/jpeg',
+        imageWidth: '1920',
+        imageHeight: '1024',
+        title: 'Fafnur - Senior Front-end Developer, Angular evangelist, Nx apologist, NodeJS warlock',
+        description: 'Angular евангелист, Nx апологет, NodeJS чернокнижник. Пишу статьи на тему современного фронтенда и веб-разработки.',
+      } as Partial<MetaConfig>,
+    },
+    {
+      provide: METRIC_CONFIG,
+      useValue: {
+        counter: 88052285,
+        ids: ['G-SM4WX8C0M1'],
+        domains: ['fafn.ru'],
+        paths: [],
+      } as MetricConfig,
+    },
+    {
       provide: APP_INITIALIZER,
-      useFactory: () => {
+      useFactory: (metaService: MetaService, metricService: MetricService) => {
         return () => {
-          // metaService: MetaService, metricService: MetricService
-          // metaService.init();
-          // metricService.init();
+          metaService.init();
+          metricService.init();
         };
       },
       multi: true,
-      // deps: [MetaService, MetricService],
+      deps: [MetaService, MetricService],
     },
   ],
 };
