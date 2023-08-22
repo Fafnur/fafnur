2a737e46ec460b8da7a7be1b8ccfd27b63bb301f
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-22 22:02:40 +0700

    [header] Adding home link on header

---
+++ libs/ui/header/src/lib/home/home.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'fafnur-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, RouterLink],
+})
+export class HomeComponent {}
