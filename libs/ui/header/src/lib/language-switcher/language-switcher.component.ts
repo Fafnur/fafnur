b18fc8a9cc105b28385cd287243fa6c893d9e25b
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-29 17:16:09 +0700

    [language] Fixing switch language again

--- libs/ui/header/src/lib/language-switcher/language-switcher.component.ts
+++ libs/ui/header/src/lib/language-switcher/language-switcher.component.ts
@@ -1,10 +1,9 @@
-import { Platform } from '@angular/cdk/platform';
 import { NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Inject, LOCALE_ID } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-import { MetricService, WindowService } from '@fafnur/core';
+import { MetricService } from '@fafnur/core';
 
 @Component({
   selector: 'fafnur-language-switcher',
@@ -15,18 +14,13 @@ import { MetricService, WindowService } from '@fafnur/core';
   imports: [MatIconModule, MatButtonModule, NgIf],
 })
 export class LanguageSwitcherComponent {
-  constructor(
-    private readonly platform: Platform,
-    private readonly windowService: WindowService,
-    private readonly metricService: MetricService,
-    @Inject(LOCALE_ID) public readonly localeId: string
-  ) {}
+  constructor(private readonly metricService: MetricService, @Inject(LOCALE_ID) public readonly localeId: string) {}
+
+  get path(): string {
+    return `/${this.localeId === 'ru' ? 'en' : 'ru'}`;
+  }
 
   onToggle(): void {
-    if (this.platform.isBrowser) {
-      const lang = this.localeId === 'ru' ? 'en' : 'ru';
-      this.metricService.send('toggle-lang', { ga: { value: lang }, ym: { lang } });
-      this.windowService.window.location.href = `${this.windowService.window.location.origin}/${lang}`;
-    }
+    this.metricService.send('toggle-lang');
   }
 }
