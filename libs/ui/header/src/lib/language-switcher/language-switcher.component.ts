610569e9d4910b440a666882c4a1c6403cb66cec
Aleksandr Serenko <fafnur@yandex.ru>
2023-08-30 10:50:14 +0700

    Revert "[language] Fixing switch language"
    This reverts commit 52f2b0bf80a34447d274e96a73ae741d247d1c9d.

--- libs/ui/header/src/lib/language-switcher/language-switcher.component.ts
+++ libs/ui/header/src/lib/language-switcher/language-switcher.component.ts
@@ -1,32 +1,61 @@
 import { Platform } from '@angular/cdk/platform';
 import { NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, LOCALE_ID } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { tap } from 'rxjs';
 
-import { MetricService, WindowService } from '@fafnur/core';
+import { CookieService, MetricService, WindowService } from '@fafnur/core';
 
 @Component({
   selector: 'fafnur-language-switcher',
+  standalone: true,
   templateUrl: './language-switcher.component.html',
   styleUrls: ['./language-switcher.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: true,
   imports: [MatIconModule, MatButtonModule, NgIf],
 })
-export class LanguageSwitcherComponent {
+export class LanguageSwitcherComponent implements OnInit {
+  control!: FormControl<boolean>;
+  isRussian = true;
+
   constructor(
     private readonly platform: Platform,
     private readonly windowService: WindowService,
-    private readonly metricService: MetricService,
-    @Inject(LOCALE_ID) public readonly localeId: string
+    private readonly cookieService: CookieService,
+    private readonly destroyRef: DestroyRef,
+    private readonly metricService: MetricService
   ) {}
 
-  onToggle(): void {
+  get lang(): string {
+    return this.isRussian ? 'ru' : 'en';
+  }
+
+  ngOnInit(): void {
     if (this.platform.isBrowser) {
-      const lang = this.localeId === 'ru' ? 'en' : 'ru';
-      this.metricService.send('toggle-lang', { ga: { value: lang }, ym: { lang } });
-      this.windowService.window.location.href = `${this.windowService.window.location.origin}/${lang}`;
+      const prefersRussian = /^ru\b/.test(this.windowService.window.navigator.language);
+      const themeLang = this.cookieService.get('themeLang');
+
+      this.isRussian = themeLang ? themeLang === 'ru' : prefersRussian ?? true;
+      this.control = new FormControl<boolean>(this.isRussian, { nonNullable: true });
+
+      this.control.valueChanges
+        .pipe(
+          tap((isRussian) => {
+            this.isRussian = isRussian;
+            this.cookieService.set('themeLang', this.lang);
+          }),
+          takeUntilDestroyed(this.destroyRef)
+        )
+        .subscribe();
     }
+  }
+
+  onToggle(): void {
+    this.control.patchValue(!this.isRussian);
+    this.metricService.send('toggle-lang');
+    this.windowService.window.location.href = `${this.windowService.window.location.origin}/${this.lang}`;
   }
 }
