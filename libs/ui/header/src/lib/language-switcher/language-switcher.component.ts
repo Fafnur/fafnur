f2d52d44fd80fcd9138374ff0ef3e7f4ebd864f0
Aleksandr Serenko <fafnur@yandex.ru>
2023-10-20 16:31:42 +0700

    [language] Adding switcher

---
+++ libs/ui/header/src/lib/language-switcher/language-switcher.component.ts
@@ -0,0 +1,30 @@
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject, LOCALE_ID } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MetricService } from '@fafnur/core';
+
+@Component({
+  selector: 'fafnur-language-switcher',
+  templateUrl: './language-switcher.component.html',
+  styleUrls: ['./language-switcher.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, NgIf],
+})
+export class LanguageSwitcherComponent {
+  constructor(private readonly metricService: MetricService, @Inject(LOCALE_ID) private readonly localeId: string) {}
+
+  get lang(): string {
+    return this.localeId === 'ru' ? 'en' : 'ru';
+  }
+
+  get path(): string {
+    return `/${this.lang}`;
+  }
+
+  onToggle(): void {
+    this.metricService.send('toggle-lang');
+  }
+}
