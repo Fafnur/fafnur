2674665aa678b29df388aca55daa88bda37a8a2d
Aleksandr Serenko <fafnur@yandex.ru>
2026-03-16 17:26:30 +0700

    [ui] Fixing logic

--- libs/ui/buttons/src/lib/button.ts
+++ libs/ui/buttons/src/lib/button.ts
@@ -8,7 +8,8 @@ import { Color } from '@fafnur/ui/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'inline-flex items-center gap-2 rounded-lg px-6 py-3 text-sm text-white font-semibold transition',
-    '[class.bg-yellow-600.hover:bg-yellow-700]': `$color() === 'primary'`,
+    '[class.bg-yellow-600]': `$color() === 'primary'`,
+    '[class.hover:bg-yellow-700]': `$color() === 'primary'`,
   },
 })
 export class Button {
